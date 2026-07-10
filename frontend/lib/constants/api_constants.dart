3e4752388dfa700ddc3cb35409654b545da95ec3
Utkarsh Raj <utkarshraj0303@gmail.com>
2026-07-10 16:49:24 +0530

    Update API base URL to Render backend

--- frontend/lib/constants/api_constants.dart
+++ frontend/lib/constants/api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://192.168.1.2:5000/api';
+  // static const String baseUrl = 'http://192.168.1.3:5000/api';
+  static const String baseUrl = 'https://notes-api-eg33.onrender.com/api';
 
   
   static const String register = '$baseUrl/auth/register';
